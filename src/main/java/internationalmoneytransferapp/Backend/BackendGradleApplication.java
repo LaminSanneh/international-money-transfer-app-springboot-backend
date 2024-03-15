b993be9fde83f78fe9e41ec6f4b90cd027bcace1
Lamin Sanneh <lamin.evra@gmail.com>
2024-03-15 10:51:54 +0000

    Initial Commit

---
+++ src/main/java/internationalmoneytransferapp/Backend/BackendGradleApplication.java
@@ -0,0 +1,13 @@
+package internationalmoneytransferapp.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/internationalmoneytransferapp/Backend/BackendGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package internationalmoneytransferapp.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
