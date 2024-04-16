10aa7824df4692ede0157f852c7c8da7866e642d
Lamin Sanneh <lamin.evra@gmail.com>
2024-04-16 11:44:49 +0000

    Add config constants

---
+++ src/main/java/internationalmoneytransferapp/Backend/config/ConfigConstants.java
@@ -0,0 +1,6 @@
+package internationalmoneytransferapp.Backend.config;
+
+public class ConfigConstants {
+    public static final Integer JWT_EXPIRATION = 700000000;
+    public static final String JWT_SECRET = "=========secretkey================secretkey================secretkey================secretkey================secretkey================secretkey=======";
+}
